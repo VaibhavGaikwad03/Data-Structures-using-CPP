b88db127bde2c8a875af7bbc4ab15f3e7c77a400
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-03-19 18:40:39 +0530

    Add files via upload

--- Linked_List/Singly_Linear/Linked_List.cpp
+++ Linked_List/Singly_Linear/Linked_List.cpp
@@ -35,7 +35,7 @@ public:
     int search_first_occurrence(int iKey);
     int search_last_occurrence(int iKey);
     int search_all_occurrences(int iKey);
-    void concat_list(LinkedList *pList);
+    void concat_list(LinkedList &pList);
     void concat_at_position(LinkedList *pList, int iPos);
     void physical_reverse();
     void reverse_display();
@@ -248,6 +248,8 @@ int LinkedList::delete_last()
 int LinkedList::delete_at_position(int iPos)
 {
     int iDelData = 0;
+    Node *pTemp = NULL;
+    Node *pTempDelete = NULL;
 
     if(iPos < 1 || iPos > m_iCount)
         return -1;
@@ -255,26 +257,186 @@ int LinkedList::delete_at_position(int iPos)
     if(iPos == 1)
         return delete_first();
 
-    
+    pTemp = m_pHead;
+
+    for (int i = 1; i < iPos - 1; i++)
+        pTemp = pTemp->m_pNext;
+
+    pTempDelete = pTemp->m_pNext;
+    iDelData = pTempDelete->m_iData;
+    pTemp->m_pNext = pTempDelete->m_pNext;
+    pTempDelete->m_pNext = NULL;
+    delete pTempDelete;
+    pTempDelete = NULL;
+
+    m_iCount--;
+
+    return iDelData;
+}
+
+int LinkedList::search_first_occurrence(int iKey)
+{
+    Node *pTemp = NULL;
+    int iCountPos = 0;
+
+    pTemp = m_pHead;
+
+    while(pTemp != NULL)
+    {
+        iCountPos++;
+        if(pTemp->m_iData == iKey)
+            break;
+        pTemp = pTemp->m_pNext;
+    }
+
+    if(pTemp == NULL)
+        return 0;
+
+    return iCountPos;
+}
+
+int LinkedList::search_last_occurrence(int iKey)
+{
+    Node *pTemp = NULL;
+    int iCountPos = 0;
+    int iRetCount = 0;
+
+    pTemp = m_pHead;
+
+    while(pTemp != NULL)
+    {
+        iCountPos++;
+        if(pTemp->m_iData == iKey)
+            iRetCount = iCountPos;
+        pTemp = pTemp->m_pNext;
+    }
+
+    return iRetCount;
+}
+
+int LinkedList::search_all_occurrences(int iKey)
+{
+    Node *pTemp = NULL;
+    int iCountOcc = 0;
+
+    pTemp = m_pHead;
+
+    while(pTemp != NULL)
+    {
+        if(pTemp->m_iData == iKey)
+            iCountOcc++;
+        pTemp = pTemp->m_pNext;
+    }
+    return iCountOcc;
+}
+
+// void LinkedList::concat_list(LinkedList *pList)
+// {
+//     Node *pTemp = NULL;
+
+//     if(pList->m_pHead == NULL)
+//         return;
+
+//     if(m_pHead == NULL)
+//     {
+//         m_pHead = pList->m_pHead;
+//         pList->m_pHead = NULL;
+//         return;
+//     }
+
+//     pTemp = m_pHead;
+
+//     while(pTemp->m_pNext != NULL)
+//         pTemp = pTemp->m_pNext;
+
+//     pTemp->m_pNext = pList->m_pHead;
+//     pList->m_pHead = NULL;
+// }
+
+void LinkedList::concat_list(LinkedList &pList)
+{
+    Node *pTemp = NULL;
+
+    if(pList.m_pHead == NULL)
+        return;
+
+    if(m_pHead == NULL)
+    {
+        m_pHead = pList.m_pHead;
+        pList.m_pHead = NULL;
+        return;
+    }
+
+    pTemp = m_pHead;
+
+    while(pTemp->m_pNext != NULL)
+        pTemp = pTemp->m_pNext;
+
+    pTemp->m_pNext = pList.m_pHead;
+    pList.m_pHead = NULL;
+    m_iCount += pList.m_iCount;
+}
+
+void LinkedList::concat_at_position(LinkedList *pList, int iPos)
+{
+    Node *pTemp1 = NULL;
+    Node *pTemp2 = NULL;
+
+    if(iPos < 1 || iPos > m_iCount+1)
+    {
+        cout << "Position is invalid\n";
+        return;
+    }
+
+    if(pList->m_pHead == NULL)
+        return;
+
+    if(iPos == 1)
+    {
+        pList->concat_list(*this);
+        m_pHead = pList->m_pHead;
+        pList->m_pHead = NULL;
+        return;
+    }
+
+    pTemp1 = m_pHead;
+
+    for (int i = 1; i < iPos - 1; i++)
+        pTemp1 = pTemp1->m_pNext;
+
+    pTemp2 = pList->m_pHead;
+
+    while (pTemp2->m_pNext != NULL)
+        pTemp2 = pTemp2->m_pNext;
+
+    pTemp2->m_pNext = pTemp1->m_pNext;
+    pTemp1->m_pNext = pList->m_pHead;
+
+    pTemp1 = pTemp2 = pList->m_pHead = NULL;
+
+    m_iCount += pList->m_iCount;
 }
 
 int main(void)
 {
-    LinkedList list;
-    list.insert_last(10);
-    list.insert_last(20);
-    list.insert_last(30);
-    list.insert_last(40);
-    list.display();
-    cout << list.count_nodes() << endl;
-    list.delete_last();
-    list.display();
-    list.delete_last();
-    list.display();
-    list.delete_last();
-    list.display();
-    list.delete_last();
-    list.display();
+    LinkedList list1;
+    LinkedList list2;
+    list1.insert_last(10);
+    list1.insert_last(20);
+    list1.insert_last(30);
+    list1.insert_last(40);
+
+    list2.insert_last(50);
+    list2.insert_last(60);
+    list2.insert_last(70);
+    list2.insert_last(80);
+
+    list1.display();
+    list2.display();
+
+    list1.concat_at_position(&list2, 3);
+    list1.display();
+    list2.display();
 
     return 0;
 }
