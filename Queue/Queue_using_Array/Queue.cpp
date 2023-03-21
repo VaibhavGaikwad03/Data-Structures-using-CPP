8391a9d5f87501d2019f5752341581d40db30519
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-03-21 09:35:59 +0530

    Add files via upload

---
+++ Queue/Queue_using_Array/Queue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+#define MAX 10
+
+class Queue
+{
+    int m_Queue[MAX];
+    int m_iCount;
+
+public:
+    Queue()
+    {
+
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
