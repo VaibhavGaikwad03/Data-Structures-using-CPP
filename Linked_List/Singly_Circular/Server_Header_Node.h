a6e47296322caf904ea4cb64e8159630c322573c
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-05-22 11:42:14 +0530

    Add files via upload

---
+++ Linked_List/Singly_Circular/Server_Header_Node.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_NODE
+#define HEADER_NODE
+
+#include <iostream>
+#include "Server_Header_Linked_List.h"
+using std::ostream;
+
+class LinkedList;
+
+class Node
+{
+    int m_iData;
+    Node *m_pNext;
+
+public:
+    Node();
+    ~Node();
+
+    friend class LinkedList;
+    friend ostream &operator<<(ostream &, LinkedList &);
+};
+
+#endif // HEADER_NODE
